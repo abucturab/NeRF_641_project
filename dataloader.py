29456126e18af41348e140670f55768acd45b514
abucturab <abucturab@gmail.com>
2023-05-03 19:14:23 -0500

    removed the commented part of the script

--- dataloader.py
+++ dataloader.py
@@ -5,79 +5,7 @@ from tqdm import tqdm
 import random
 import argparse
 import sys
-# class dataloader:
-#     def __init__(self, location):
-#         self.location = location
-#         # Using dict() constructor
-#         self.my_dict = dict()
 
-#     def write2dict(self, typeval="pose"):
-        
-#         frames_value = []
-
-#         dirpath = os.path.join(self.location, typeval)
-
-#         for filename in tqdm(os.listdir(dirpath)):
-#             file_path = os.path.join(dirpath, filename)
-            
-#             if os.path.isfile(file_path):
-#                 #print(f"Reading file: {file_path}")
-
-#                 scene_pose_matrix = []
-#                 with open(file_path, 'r') as f:
-#                     line = f.readline().strip()
-#                     values = [float(x) for x in line.split()]
-#                     if len(values) != 16:
-#                         print(f"Invalid number of values in {filename}. Skipping.")
-#                         continue
-
-#                     transform_matrix_entry = [values[i:i + 4] for i in range(0, 16, 4)]
-#                     basename = os.path.splitext(os.path.basename(file_path))[0]
-#                     filepath_entry = os.path.join(self.location, f"rgb/{basename}")
-#                     rotation = 0.012566370614359171
-#                     frame_dict = dict()
-#                     frame_dict["file_path"] = filepath_entry
-#                     frame_dict["rotation"] = rotation
-#                     frame_dict["transform_matrix"] = transform_matrix_entry
-#                     frames_value.append(frame_dict)
-        
-#         self.my_dict["camera_angle_x"] = 0.6194058656692505
-#         self.my_dict["frames"] = frames_value
-    
-#     def write2json(self, jsonPath):
-#         # Open a file for writing
-#         with open(jsonPath, 'w') as f:
-#             # Write the dictionary to the file in JSON format
-#             json.dump(self.my_dict, f)
-
-#     def __call__(self):
-#         self.write2dict()
-#         os.system("touch transforms.json")
-#         self.write2json(os.path.join(os.getcwd(), "transforms.json"))
-
-#     def load(self):
-#         pass
-
-
-# os.system("mkdir val train")
-
-# # # Get a list of all files in the source directory
-# all_files = os.listdir(os.path.join(os.getcwd(), "c951be855f20a1bfb2a29f45da811562/rgb"))
-# # random.shuffle(all_files)
-
-# # for i,target_file in enumerate(all_files):
-# #     file_name = os.path.splitext(target_file)[0]
-# #     pic_file = os.path.join(os.getcwd(), f"c951be855f20a1bfb2a29f45da811562/rgb/{file_name}.png")
-# #     if i < 200:
-# #         pic_target_path = os.path.join(os.getcwd(), "train")
-# #     else:
-# #         pic_target_path = os.path.join(os.getcwd(), "val")
-# #     #print(pic_file)
-# #     #print(pic_target_path)
-# #     #break
-# #    os.system(f"cp {pic_file} {pic_target_path} ")
-# # loadobj = dataloader(os.path.join(os.getcwd(), "c951be855f20a1bfb2a29f45da811562"))
-# # loadobj()
 
 def getTransformDict(file_list, type="train", dir_path=None):
     my_dict = dict()
